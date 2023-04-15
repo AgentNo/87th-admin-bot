b635aadb9aba8e45c6b162a79abb6d6a7d27fa4a
AgentNo <dfaulkner2010@hotmail.co.uk>
2023-04-15 20:12:35 +0100

    Added heartbeat command

--- bot/bot.py
+++ bot/bot.py
@@ -14,7 +14,8 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.default()
 intents.members = True
-bot = discord.Client(intents=intents)
+intents.message_content = True
+bot = commands.Bot(intents=intents, command_prefix='!')
 
 
 # Start logger
@@ -28,14 +29,28 @@ log.addHandler(handler)
 @bot.event
 async def on_ready():
     log.info(f'{bot.user} has connected to the following guilds: ' + ', '.join(guild.name for guild in bot.guilds))
-    for guild in bot.guilds:
-        log.info(guild.name)
 
 
 @bot.event
 async def on_member_join(member):
     await member.create_dm()
     await member.dm_channel.send(strings.DM_WELCOME_MESSAGE.format(member.name))
+
+
+# !ping - Health command to ensure bot is responsive to commands and currently operational
+@bot.command(name="heartbeat")
+async def ping(ctx):
+    await ctx.channel.send("87th Admin Bot is up and running!")
+
+
+# !enlist <user> - Will give the user basic Recruit roles,
+@bot.command(name="enlist")
+async def enlist_member(ctx, *args):
+    response = ""
+    for arg in args:
+        response = response + " " + arg
+
+    await ctx.channel.send(response)
 
 
 # Run the bot
