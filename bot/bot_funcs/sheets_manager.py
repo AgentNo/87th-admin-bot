bf090d694896f493c94fb91ae3216cb9cbccfdd4
AgentNo <dfaulkner2010@hotmail.co.uk>
2023-10-08 11:25:43 +0100

    Added note on local running

--- bot/bot_funcs/sheets_manager.py
+++ bot/bot_funcs/sheets_manager.py
@@ -19,6 +19,7 @@ async def get_master_doc_secrets():
 
 
 # Get and return the connection object for the master doc
+# TO RUN LOCALLY: replace the filename arg with 'sheets_keys.json'
 async def get_master_doc_connection(SHEETS_KEY):
     gc = gspread.service_account(filename='87th-admin-bot/bot/sheets_keys.json')
     gdoc = gc.open_by_key(SHEETS_KEY)
