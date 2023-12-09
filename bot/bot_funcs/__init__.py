9e60e21150056615bde06d086426fdbc58c74249
AgentNo <dfaulkner2010@hotmail.co.uk>
2023-12-09 18:14:04 +0000

    Removed useless import

--- bot/bot_funcs/__init__.py
+++ bot/bot_funcs/__init__.py
@@ -1,6 +1,5 @@
 # Python package for bot command and functionality 
 from bot_funcs.message_listener import add_or_remove_signup_reactions
-from bot_funcs.message_listener import check_if_message_has_femboy
 from bot_funcs.dm_manager import send_dm_to_new_member
 from bot_funcs.commands.enlist_member import enlist_member
 from bot_funcs.commands.grant_role import grant_role
