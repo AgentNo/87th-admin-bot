20d5d89d4af8732930479532f5b29691c882f9dc
AgentNo <dfaulkner2010@hotmail.co.uk>
2023-12-09 18:32:03 +0000

    Tidy up of !enlist

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ Note: This file was started on 2023-04-22. Any changes before then are not captu
 - Changed name of signup function to better reflect its purpose (`check_and_put_signup_reactions` -> `add_or_remove_signup_reactions`)
 - Removed the femboy easter egg. Was a joke that overstayed its welcome
 - Removed some pointless conditions from the the community roles functionality
+- Tidied up the !enlist function
 
 ## 2023-12-01
 - Introduced error handling into dm_message listener and improved logging
--- bot/bot_funcs/commands/enlist_member.py
+++ bot/bot_funcs/commands/enlist_member.py
@@ -6,23 +6,29 @@ import utility.enums as enums
 import utility.strings as strings
 from utility.setup_logger import logger
 
+
 async def enlist_member(ctx, user: discord.Member):
     logger.info(f'Enlist command triggered by user {ctx.author.name} ({ctx.author.id}) for {user.display_name} ({user.id})')
     if None != ctx.guild.get_member(user.id).get_role(enums.GRANTABLE_ROLES["regiment"]):
-        await ctx.channel.send(f':x: I can\'t do that <@{ctx.author.id}> - it looks like <@{user.id}> is already enlisted!')
+        await ctx.channel.send(f':x: I can\'t do that <@{ctx.author.id}> - it looks like this member is already enlisted!')
     else: 
-        await ctx.channel.send(f"Enlisting user, sit tight...")
+        await ctx.channel.send(f"Enlisting member, sit tight...")
+
         if None != ctx.guild.get_member(user.id).get_role(enums.GRANTABLE_ROLES["unassigned"]):
-            await ctx.guild.get_member(user.id).remove_roles(ctx.guild.get_role(enums.GRANTABLE_ROLES["unassigned"])) # Remove the 'Unassigned' role
+            await ctx.guild.get_member(user.id).remove_roles(ctx.guild.get_role(enums.GRANTABLE_ROLES["unassigned"]))
+
         for id in enums.GRANTABLE_ROLES["enlistment"]:
             try:
                 role = ctx.guild.get_role(id)
                 await ctx.guild.get_member(user.id).add_roles(role)
             except Exception as e:
                 await ctx.channel.send(f'Error running command !enlist - {e}. Command failed on role {role}, id = {id}')
-        if len(user.display_name) < 19:
-            await user.edit(nick=f'[87th] Rec. | {user.display_name}')
-            await ctx.channel.send(strings.ENLIST_SUCCESS_MESSAGE.format(user.id))
-        else:
-            await ctx.channel.send(strings.ENLIST_SUCCESS_MESSAGE.format(user.id))
-            await ctx.channel.send(f'Sorry <@{ctx.author.id}>, I could not change this user\'s name as it is longer than 19 characters :pensive:.')
+        await update_recruit_nick(ctx, user)
+
+
+async def update_recruit_nick(ctx, user):
+    await ctx.channel.send(strings.ENLIST_SUCCESS_MESSAGE.format(user.id))
+    if len(user.display_name) < 19:
+        await user.edit(nick=f'[87th] Rec. | {user.display_name}')
+    else:
+        await ctx.channel.send(f':pensive: Sorry <@{ctx.author.id}>, I could not change this member\'s name as it is longer than 19 characters.')
